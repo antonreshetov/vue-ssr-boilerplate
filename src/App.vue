35a64f7d3a5b8177913cee1406b0ada1ebf9ce5a
Rafael Pedrola Gimeno <rafinskipg@gmail.com>
2019-02-11 16:22:13 +0100

    feat: use router view (#18)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Hello, World!" />
+    <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  components: {
-    HelloWorld : () => import('./components/HelloWorld.vue'),
-  }
+  components: {}
 }
 </script>
 
