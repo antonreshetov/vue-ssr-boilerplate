dd6cb790893f98307f00b1eed7479eac998c112c
eddyerburgh <edward.yerburgh@gmail.com>
2019-02-17 20:05:56 +0000

    refactor: change title

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('*', (req, res) => {
   res.setHeader("Content-Type", "text/html")
 
   const context = {
-    title: 'Vue HN 2.0', // default title
+    title: 'Vue CLI 3 SSR example',
     url: req.url
   }
 
