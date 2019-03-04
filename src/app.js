4254c6178666cbf0aa90393ca2e775c7d56922c7
Anton Reshetov <reshetov.art@gmail.com>
2019-03-04 10:03:01 +0300

    feat: add store, sync store & router

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
+import { createStore } from './store'
+import { sync } from 'vuex-router-sync'
 
 export function createApp () {
   const router = createRouter()
+  const store = createStore()
+
+  sync(store, router)
 
   const app = new Vue({
     router,
+    store,
     render: h => h(App)
   })
 
-  return { app, router }
+  return { app, router, store }
 }
